cfdbbd960903d815a0982969cbfe198d06a12daf
mariskakassi <146140221+mariskakassi@users.noreply.github.com>
2024-10-22 17:46:21 +0000

    square root working

--- app.rb
+++ app.rb
@@ -1,9 +1,40 @@
 require "sinatra"
+require "http"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  @num = params.fetch("number").to_i
+  @result = @num * @num
+  erb(:square_result)
+end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get("/square_root/results") do
+  erb(:square_root_result)
+end
+
+get("payment/new") do
+  erb(:payment_new)
+end
+
+get("payment/results") do
+  erb(:payment_result)
+end
+
+get("random/new") do
+  erb(:random_new)
+end
+
+get("random/results") do
+  erb(:random_result)
 end
