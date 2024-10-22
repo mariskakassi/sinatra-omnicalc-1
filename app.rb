9ce30e3e4700ec34922570eb34c9fca82fe6b11c
mariskakassi <146140221+mariskakassi@users.noreply.github.com>
2024-10-22 18:14:31 +0000

    payment working

--- app.rb
+++ app.rb
@@ -25,18 +25,26 @@ get("/square_root/results") do
   erb(:square_root_result)
 end
 
-get("payment/new") do
+get("/payment/new") do
   erb(:payment_new)
 end
 
-get("payment/results") do
+get("/payment/results") do
+  @apr = params.fetch("apr").to_f
+  @yrs = params.fetch("years").to_f
+  @princi = params.fetch("principal").to_f
+
+  r = (@apr / 100.0) / 12.0
+  n = @yrs * 12
+  p = @princi
+  @pay = (r * p) / (1 - ((1 + r)**(-n)))
   erb(:payment_result)
 end
 
-get("random/new") do
+get("/random/new") do
   erb(:random_new)
 end
 
-get("random/results") do
+get("/random/results") do
   erb(:random_result)
 end
